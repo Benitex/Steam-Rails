8b3d00f07b2d289210a494ae3d81a39800968897
Benitex <benitoapepe@yahoo.com.br>
2023-07-05 22:15:17 -0300

    Add enemies sprites

--- data/enemies.py
+++ data/enemies.py
@@ -2,9 +2,60 @@ from pygame import image
 from scripts.enemies.enemy_type import EnemyType
 from data.items import *
 
-ENEMY_EXAMPLE = EnemyType(
-  width = 64,
-  height = 64,
+BAT = EnemyType(
+  spritesheet = image.load("graphics/enemies/bat.png"),
+  number_of_frames = 4,
+  width = 16,
+  height = 16,
+  damage = 1,
+  attack_cooldown = 300,
+  knockback_intensity = 0.15,
+  health = 3,
+  speed = 0.07,
+  drops = [
+    [TOOTH, 10],
+    [BELT, 15],
+    [EGGS, 20],
+  ],
+)
+
+RAT = EnemyType(
+  spritesheet = image.load("graphics/enemies/rat.png"),
+  number_of_frames = 6,
+  width = 16,
+  height = 16,
+  damage = 1,
+  attack_cooldown = 300,
+  knockback_intensity = 0.15,
+  health = 3,
+  speed = 0.07,
+  drops = [],
+)
+
+WOLF = EnemyType(
+  spritesheet = image.load("graphics/enemies/wolf.png"),
+  number_of_frames = 6,
+  width = 48,
+  height = 16,
+  sprite_y_offset = 16,
+  damage = 1,
+  attack_cooldown = 300,
+  knockback_intensity = 0.15,
+  health = 3,
+  speed = 0.2,
+  run_duration = 1200,
+  drops = [
+    [TEDDY_BEAR, 10],
+    [TRUMPET, 15],
+    [EGGS, 20],
+  ],
+)
+
+GOLEM = EnemyType(
+  spritesheet = image.load("graphics/enemies/golem.png"),
+  number_of_frames = 4,
+  width = 32,
+  height = 32,
   damage = 2,
   attack_cooldown = 400,
   knockback_intensity = 0.3,
@@ -15,34 +66,14 @@ ENEMY_EXAMPLE = EnemyType(
     [PINK_ROSE, 15],
     [HONEY, 15],
   ],
-  spritesheet = image.load("placeholder/graphics/enemies/Eyegore_Statue.png"),
-  number_of_frames = 10,
-  sprite_y_offset = 32,
 )
 
-ENEMY_EXAMPLE_2 = EnemyType(
-  spritesheet = image.load("placeholder/graphics/enemies/snake.png"),
+SLIME = EnemyType(
+  spritesheet = image.load("graphics/enemies/slime.png"),
   number_of_frames = 4,
-  width = 48,
-  height = 32,
-  damage = 1,
-  attack_cooldown = 300,
-  knockback_intensity = 0.2,
-  health = 2,
-  speed = 0.1,
-  run_duration = 1200,
-  drops = [
-    [TEDDY_BEAR, 10],
-    [TRUMPET, 15],
-    [EGGS, 20],
-  ],
-)
-
-ENEMY_EXAMPLE_3 = EnemyType(
-  spritesheet = image.load("placeholder/graphics/enemies/evil_enemy.png"),
-  number_of_frames = 3,
   width = 32,
-  height = 32,
+  height = 16,
+  sprite_y_offset = 16,
   damage = 0,
   attack_cooldown = 200,
   knockback_intensity = 1,
@@ -56,27 +87,4 @@ ENEMY_EXAMPLE_3 = EnemyType(
   ],
 )
 
-ENEMY_EXAMPLE_4 = EnemyType(
-  spritesheet = image.load("placeholder/graphics/enemies/enemy.png"),
-  number_of_frames = 3,
-  sprite_y_offset = 16,
-  width = 32,
-  height = 32,
-  damage = 1,
-  attack_cooldown = 300,
-  knockback_intensity = 0.15,
-  health = 3,
-  speed = 0.07,
-  drops = [
-    [TOOTH, 10],
-    [BELT, 15],
-    [EGGS, 20],
-  ],
-)
-
-enemy_types_list = [
-  ENEMY_EXAMPLE,
-  ENEMY_EXAMPLE_2,
-  ENEMY_EXAMPLE_3,
-  ENEMY_EXAMPLE_4,
-]
+enemy_types_list = [GOLEM, WOLF, SLIME, BAT, RAT]
